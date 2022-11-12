a6bd7bb148316fc1811d54734d7b6ddbe035d249
unknown <sakshiarya451@gmail.com>
2022-11-12 21:29:47 +0530

    added project

---
+++ core/src/main/java/com/aem/assignment/pwc/core/models/ShowHideModel.java
@@ -0,0 +1,25 @@
+package com.aem.assignment.pwc.core.models;
+
+import java.util.List;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = { Resource.class })
+public interface ShowHideModel {
+	@ChildResource(injectionStrategy = InjectionStrategy.OPTIONAL, name = "names")
+	List<Name> getNames();
+
+	@Model(adaptables = Resource.class)
+	interface Name {
+
+		@ValueMapValue(name = "firstname")
+		String getFirstName();
+
+		@ValueMapValue(name = "lastname")
+		String getLastName();
+	}
+}
